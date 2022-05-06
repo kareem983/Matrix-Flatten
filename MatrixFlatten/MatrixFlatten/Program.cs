9b311f30c2c4c927a7e4e3a9570ee105a82f2d09
kareem saeed <karimsaid026@gmail.com>
2022-05-06 02:27:14 +0200

    Adding the testing matrix function.

--- MatrixFlatten/MatrixFlatten/Program.cs
+++ MatrixFlatten/MatrixFlatten/Program.cs
@@ -12,7 +12,7 @@ namespace MatrixFlatten
         {
             userInput();
             create1DArray();
-
+            testMatrixFunctions();
         }
 
         #region MATRIX FUNCTIONS
@@ -52,6 +52,49 @@ namespace MatrixFlatten
         static int getMirrorIndex(int i, int j, int k)
         {
             return (i * m * p) + (j * p) + k;
+        }
+
+        static void testMatrixFunctions()
+        {
+            // this to make sure that the 1DArray is Created Successfully
+            Console.Write("\n\n### 1D Array Created Successfully ###\nThe 1D Array Values: ");
+            for (int y = 0; y < (n * m * p); y++)
+                Console.Write(arr_1D[y] + " ");
+
+
+            // this to make sure that the mirror index is true
+            char cohice = 'n';
+            do
+            {
+                Console.WriteLine("\n\n______________________________________________________________________");
+                Console.WriteLine("Enter The Indices of 3D Array to get the Mirror Index from 1D Array");
+                int testI_index, testJ_index, testK_index;
+                Console.WriteLine("Enter The index I in 3d Array: ");
+                testI_index = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter The index J in 3d Array: ");
+                testJ_index = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter The index K in 3d Array: ");
+                testK_index = int.Parse(Console.ReadLine());
+
+                try
+                {
+                    if (testI_index >= n || testJ_index >= m || testK_index >= p) throw new Exception();
+
+                    int MirrorIndex = getMirrorIndex(testI_index, testJ_index, testK_index);
+                    Console.WriteLine("The Mirror index from 1D Array: " + MirrorIndex);
+                    Console.WriteLine($"The Value of Mirror index [{MirrorIndex}] from 1D Array: " + arr_1D[MirrorIndex]);
+                    Console.WriteLine($"The Value of index [{testI_index}, {testJ_index}, {testK_index}] from 3D Array: "
+                        + arr_3D[testI_index, testJ_index, testK_index]);
+                }
+                catch
+                {
+                    Console.WriteLine($"Please Enter Indexes in range of 3D Array I[0-{n - 1}], J[0-{m - 1}], K[0-{p - 1}]!!!");
+                }
+                Console.WriteLine("______________________________________________________________________");
+                Console.WriteLine("\nDo you want to test another test(y/n)");
+                cohice = char.Parse(Console.ReadLine());
+
+            } while (cohice == 'y' || cohice == 'Y');
         }
         #endregion
 
