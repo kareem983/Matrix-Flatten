65de11b4e5802c5972eba1c691dce6c3e35443b6
kareem saeed <karimsaid026@gmail.com>
2022-05-10 22:53:12 +0200

    Update Variables Naming Convention.

--- MatrixFlatten/MatrixFlatten/Program.cs
+++ MatrixFlatten/MatrixFlatten/Program.cs
@@ -6,7 +6,7 @@ namespace MatrixFlatten
     {
         static int [] arr_1D;
         static int[ , , ] arr_3D;
-        static int n, m, p, q;
+        static int rows_3dArr, cols_3dArr, size_thirdD3dArr, size_1dArr;
 
         static void Main(string[] args)
         {
@@ -19,19 +19,19 @@ namespace MatrixFlatten
         static void userInput()
         {
             Console.WriteLine("Enter The number of Rows for 3d Array: ");
-            n = int.Parse(Console.ReadLine());
+            rows_3dArr = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter The number of Columns for 3d Array: ");
-            m = int.Parse(Console.ReadLine());
+            cols_3dArr = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter The number of Cells in each Cell for 3d Array: ");
-            p = int.Parse(Console.ReadLine());
-            arr_3D = new int[n, m, p];
+            size_thirdD3dArr = int.Parse(Console.ReadLine());
+            arr_3D = new int[rows_3dArr, cols_3dArr, size_thirdD3dArr];
             Console.WriteLine("\n======================================================================");
 
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < m; j++)
+            for (int i = 0; i < rows_3dArr; i++)
+                for (int j = 0; j < cols_3dArr; j++)
                 {
                     Console.WriteLine($"The Row number {i + 1}, The Column number: {j + 1}, Enter The Values of this Cell: ");
-                    for (int k = 0; k < p; k++)
+                    for (int k = 0; k < size_thirdD3dArr; k++)
                         arr_3D[i, j, k] = int.Parse(Console.ReadLine());
                 }
             Console.WriteLine("======================================================================");
@@ -40,25 +40,26 @@ namespace MatrixFlatten
         static void create1DArray()
         {
             int y = 0;
-            q = n * m * p;
-            arr_1D = new int[q];
+            size_1dArr = rows_3dArr * cols_3dArr * size_thirdD3dArr;
+            arr_1D = new int[size_1dArr];
 
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < m; j++)
-                    for (int k = 0; k < p; k++)
+            for (int i = 0; i < rows_3dArr; i++)
+                for (int j = 0; j < cols_3dArr; j++)
+                    for (int k = 0; k < size_thirdD3dArr; k++)
                         arr_1D[y++] = arr_3D[i, j, k];
         }
 
-        static int getMirrorIndex(int i, int j, int k)
+        static int getMirrorIndex(int index_1dim, int index_2dim, int index_3dim)
         {
-            return (i * m * p) + (j * p) + k;
+            // getMirrorIndex: take indices of 3D Array and return the mirror index from 1d Array, Using the below Equation.
+            return (index_1dim * cols_3dArr * size_thirdD3dArr) + (index_2dim * size_thirdD3dArr) + index_3dim;
         }
 
         static void testMatrixFunctions()
         {
             // this to make sure that the 1DArray is Created Successfully
             Console.Write("\n\n### 1D Array Created Successfully ###\nThe 1D Array Values: ");
-            for (int y = 0; y < (n * m * p); y++)
+            for (int y = 0; y < (rows_3dArr * cols_3dArr * size_thirdD3dArr); y++)
                 Console.Write(arr_1D[y] + " ");
 
 
@@ -67,7 +68,7 @@ namespace MatrixFlatten
             do
             {
                 Console.WriteLine("\n\n______________________________________________________________________");
-                Console.WriteLine("Enter The Indices of 3D Array to get the Mirror Index from 1D Array");
+                Console.WriteLine($"Enter The Indices of 3D Array in ranges I[0-{rows_3dArr - 1}], J[0-{cols_3dArr - 1}], K[0-{size_thirdD3dArr - 1}]\nto get the Mirror Index from 1D Array");
                 int testI_index, testJ_index, testK_index;
                 Console.WriteLine("Enter The index I in 3d Array: ");
                 testI_index = int.Parse(Console.ReadLine());
@@ -78,7 +79,9 @@ namespace MatrixFlatten
 
                 try
                 {
-                    if (testI_index >= n || testJ_index >= m || testK_index >= p) throw new Exception();
+                    if (testI_index < 0  || testI_index >= rows_3dArr ||
+                        testJ_index < 0  || testJ_index >= cols_3dArr ||
+                        testK_index < 0  || testK_index >= size_thirdD3dArr) throw new Exception();
 
                     int MirrorIndex = getMirrorIndex(testI_index, testJ_index, testK_index);
                     Console.WriteLine("The Mirror index from 1D Array: " + MirrorIndex);
@@ -88,7 +91,7 @@ namespace MatrixFlatten
                 }
                 catch
                 {
-                    Console.WriteLine($"Please Enter Indexes in range of 3D Array I[0-{n - 1}], J[0-{m - 1}], K[0-{p - 1}]!!!");
+                    Console.WriteLine($"Please Enter Indexes in range of 3D Array I[0-{rows_3dArr - 1}], J[0-{cols_3dArr - 1}], K[0-{size_thirdD3dArr - 1}]!!!");
                 }
                 Console.WriteLine("______________________________________________________________________");
                 Console.WriteLine("\nDo you want to test another test(y/n)");
